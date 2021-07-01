1817daf4eb898b227557071743232b13610b7740
Vilian P <vilkata41@gmail.com>
2021-07-01 19:26:23 +0300

    Adding ImportantConstants to gitignore

--- src/tweetFilteredPackage/ImportantConstants.java
+++ src/tweetFilteredPackage/ImportantConstants.java
@@ -6,9 +6,9 @@ package tweetFilteredPackage;
  */
 public final class ImportantConstants {
 
-	public final static String CONSUMER_KEY = "Your consumer key here";
-	public final static String CONSUMER_SECRET = "Your consumer secret here";
-	public final static String ACCESS_TOKEN = "Your access token here";
-	public final static String TOKEN_SECRET = "Your access token secret here";
+	public final static String CONSUMER_KEY = "QY4ADzD93kp8iZ4hstDJ5TdCV";
+	public final static String CONSUMER_SECRET = "KnuO5XLxYW6qG69S5LrFBqhSh6u2gOKwt89Oxou9Pl3VatqGfJ";
+	public final static String ACCESS_TOKEN = "1405868417094148102-vBqWZuQKMjeIyOtkfGdXcfaT2584iS";
+	public final static String TOKEN_SECRET = "tdIUCgRzK4niznrYoeUVhKcYgqZkztivW5U0DnjQPXa9P";
 	
 }
