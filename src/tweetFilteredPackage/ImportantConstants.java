abc85c474d75547ca60236764f208103b1641482
Vilian P <68128862+vilkata41@users.noreply.github.com>
2021-07-01 19:11:58 +0300

    Create ImportantConstants.java

---
+++ src/tweetFilteredPackage/ImportantConstants.java
@@ -0,0 +1,14 @@
+package tweetFilteredPackage;
+
+/**
+ * @author Vilian Popov
+ *
+ */
+public final class ImportantConstants {
+
+	public final static String CONSUMER_KEY = "Your consumer key here";
+	public final static String CONSUMER_SECRET = "Your consumer secret here";
+	public final static String ACCESS_TOKEN = "Your access token here";
+	public final static String TOKEN_SECRET = "Your access token secret here";
+	
+}
