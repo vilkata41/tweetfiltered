47f1928fbe6bc9944e205956974706e09d433815
Vilian P <vilkata41@gmail.com>
2021-07-01 18:51:13 +0300

    Twitter4J configuration

---
+++ src/tweetFilteredPackage/ImportantConstants.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package tweetFilteredPackage;
+
+/**
+ * @author Vilian Work
+ *
+ */
+public final class ImportantConstants {
+
+	public final static String CONSUMER_KEY = "QY4ADzD93kp8iZ4hstDJ5TdCV";
+	public final static String CONSUMER_SECRET = "KnuO5XLxYW6qG69S5LrFBqhSh6u2gOKwt89Oxou9Pl3VatqGfJ";
+	public final static String ACCESS_TOKEN = "1405868417094148102-vBqWZuQKMjeIyOtkfGdXcfaT2584iS";
+	public final static String TOKEN_SECRET = "tdIUCgRzK4niznrYoeUVhKcYgqZkztivW5U0DnjQPXa9P";
+	
+}
--- src/tweetFilteredPackage/TwitterInfo.java
+++ src/tweetFilteredPackage/TwitterInfo.java
@@ -1,9 +1,17 @@
 package tweetFilteredPackage;
 
+import java.util.*;
+import twitter4j.*;
+import twitter4j.conf.*;
+
 public class TwitterInfo {
 
 	public static void main(String[] args) {
-
+		ConfigurationBuilder configurationBuilder = new ConfigurationBuilder();
+		configurationBuilder.setDebugEnabled(true).setOAuthConsumerKey(ImportantConstants.CONSUMER_KEY) 
+						.setOAuthConsumerSecret(ImportantConstants.CONSUMER_SECRET)
+						.setOAuthAccessToken(ImportantConstants.ACCESS_TOKEN)
+						.setOAuthAccessTokenSecret(ImportantConstants.TOKEN_SECRET);	
 	}
 
 }
