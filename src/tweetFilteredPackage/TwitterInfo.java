28e69219bf5e73ae71cf8616cfcfc6a70f915bb4
Vilian P <vilkata41@gmail.com>
2021-07-02 16:42:07 +0300

    Reading tweets that tweetfiltered was mentioned in

--- src/tweetFilteredPackage/TwitterInfo.java
+++ src/tweetFilteredPackage/TwitterInfo.java
@@ -1,5 +1,7 @@
 package tweetFilteredPackage;
 
+import java.util.List;
+
 import twitter4j.*;
 import twitter4j.conf.*;
 
@@ -10,7 +12,35 @@ public class TwitterInfo {
 		configurationBuilder.setDebugEnabled(true).setOAuthConsumerKey(ImportantConstants.CONSUMER_KEY) 	//For everything to work properly with Twitter's API
 						.setOAuthConsumerSecret(ImportantConstants.CONSUMER_SECRET)							//We'd need the consumer key and secret AND
 						.setOAuthAccessToken(ImportantConstants.ACCESS_TOKEN)								//The access token and secret.
-						.setOAuthAccessTokenSecret(ImportantConstants.TOKEN_SECRET);	
+						.setOAuthAccessTokenSecret(ImportantConstants.TOKEN_SECRET);
+		
+		Twitter twitter = new TwitterFactory(configurationBuilder.build()).getInstance(); //Getting the instance for the twitter account
+		
+		try {
+			User user = twitter.verifyCredentials(); //Getting the actual tweetfiltered account on twitter
+			List<Status> mentionedStatuses = twitter.getMentionsTimeline(); //Filling a list with the tweets we're menitioned in.
+			
+			for (Status s: mentionedStatuses) {
+				System.out.println(""); 												//Just adding a new line, so the tweets don't blend
+				System.out.println(s.getUser().getName() + " tweeted: " + s.getText());
+				
+				if(s.getMediaEntities().length > 0) {
+					System.out.println("Included media: ");
+					
+					for(int i = 0; i < s.getMediaEntities().length; i++) {
+						if(s.getMediaEntities()[i].getType() == "video") s.getMediaEntities()[i].getMediaURL();
+						else System.out.println(s.getMediaEntities()[i].getMediaURL());
+					}
+				}
+				
+			}
+			
+		}
+		catch(TwitterException ex) { //We catch the exception and handle it
+			ex.printStackTrace();
+			System.out.println("Failed to get timeline.");
+			System.exit(-1);
+		}
 	}
 
 }
