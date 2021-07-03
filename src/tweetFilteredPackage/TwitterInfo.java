280451dc3146ca322f5e09e9ac3073c65c133a54
Vilian P <vilkata41@gmail.com>
2021-07-03 12:04:23 +0300

    Adding some comments

--- src/tweetFilteredPackage/TwitterInfo.java
+++ src/tweetFilteredPackage/TwitterInfo.java
@@ -8,28 +8,29 @@ import twitter4j.conf.*;
 public class TwitterInfo {
 
 	public static void main(String[] args) {
-		ConfigurationBuilder configurationBuilder = new ConfigurationBuilder(); 							//Set up the configuration builder
-		configurationBuilder.setDebugEnabled(true).setOAuthConsumerKey(ImportantConstants.CONSUMER_KEY) 	//For everything to work properly with Twitter's API
-						.setOAuthConsumerSecret(ImportantConstants.CONSUMER_SECRET)							//We'd need the consumer key and secret AND
-						.setOAuthAccessToken(ImportantConstants.ACCESS_TOKEN)								//The access token and secret.
+		ConfigurationBuilder configurationBuilder = new ConfigurationBuilder(); 							// Set up the configuration builder
+		configurationBuilder.setDebugEnabled(true).setOAuthConsumerKey(ImportantConstants.CONSUMER_KEY) 	// For everything to work properly with Twitter's API
+						.setOAuthConsumerSecret(ImportantConstants.CONSUMER_SECRET)							// We'd need the consumer key and secret AND
+						.setOAuthAccessToken(ImportantConstants.ACCESS_TOKEN)								// The access token and secret.
 						.setOAuthAccessTokenSecret(ImportantConstants.TOKEN_SECRET);
 		
-		Twitter twitter = new TwitterFactory(configurationBuilder.build()).getInstance(); //Getting the instance for the twitter account
+		Twitter twitter = new TwitterFactory(configurationBuilder.build()).getInstance(); // Getting the instance for the twitter account.
 		
 		try {
-			User user = twitter.verifyCredentials(); //Getting the actual tweetfiltered account on twitter
-			List<Status> mentionedStatuses = twitter.getMentionsTimeline(); //Filling a list with the tweets we're menitioned in.
+			User user = twitter.verifyCredentials(); // Getting the actual tweetfiltered account on twitter.
+			List<Status> mentionedStatuses = twitter.getMentionsTimeline(); // Filling a list with the tweets we're menitioned in.
 			
 			for (Status s: mentionedStatuses) {
-				System.out.println(""); 												//Just adding a new line, so the tweets don't blend
+				System.out.println(""); 												// Just adding a new line, so the tweets don't blend.
 				
-				System.out.println(s.getUser().getName() + " tweeted: " + s.getText().toString().replaceAll("https://t.co/[a-zA-Z_0-9]*","")); //Removing the link at the end of posts with media
+				System.out.println("@" + s.getUser().getName() + " tweeted: " + s.getText()		// Getting the tweet's text.
+									.toString().replaceAll("https://t.co/[a-zA-Z_0-9]*","")); 	// Here, we remove the link after tweets with media.
 				
 				if(s.getMediaEntities().length > 0) {
 					System.out.println("Included media: ");
 					
 					for(int i = 0; i < s.getMediaEntities().length; i++) {
-						if(s.getMediaEntities()[i].getType().equals("video")) System.out.println(s.getMediaEntities()[i].getVideoVariants()[0].getUrl());
+						if(s.getMediaEntities()[i].getType().equals("video")) System.out.println(s.getMediaEntities()[i].getVideoVariants()[0].getUrl()); // We get the video in its mp4 variant
 						else System.out.println(s.getMediaEntities()[i].getMediaURL());
 					}
 				}
@@ -37,7 +38,7 @@ public class TwitterInfo {
 			}
 			
 		}
-		catch(TwitterException ex) { //We catch the exception and handle it
+		catch(TwitterException ex) { // We catch the exception and handle it.
 			ex.printStackTrace();
 			System.out.println("Failed to get tweets.");
 			System.exit(-1);
